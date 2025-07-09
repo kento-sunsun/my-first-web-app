ca3622b267ca8748010ff1298110bfa81cedbeed
kento-sunsun <tokuken1129@gmail.com>
2025-07-09 15:35:27 +0900

    ホームページからの切り替え、数の編集

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import { setupTabs } from './tabs.js';
+import { setupConverter } from './converter.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  setupTabs();
+  setupConverter();
+});
---
+++ main.js
@@ -0,0 +1,8 @@
+import { setupTabs } from "./js/tabs.js";
+import { setupConverter } from "./js/converter.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  setupTabs();
+  setupConverter();
+});
+
