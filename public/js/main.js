5fdb4cec37cb4b75d50099d9eb656fba56fc253b
kento-sunsun <tokuken1129@gmail.com>
2025-07-09 18:09:55 +0900

    Delete public directory

--- public/js/main.js
+++
@@ -1,7 +0,0 @@
-import { setupTabs } from './tabs.js';
-import { setupConverter } from './converter.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  setupTabs();
-  setupConverter();
-});
